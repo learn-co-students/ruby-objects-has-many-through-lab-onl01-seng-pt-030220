d7dc685e495b4b972139e24d9e00a6f8638873e1
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-01 06:50:39 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,15 +1,17 @@
 class Patient
+  
     attr_accessor :name
 
-    @@all =[]
-    def initialize(name)
-        @name=name
-        @@all << self
-    end
+      @@all =[]
 
-    def self.all
-      @@all
-    end
+      def initialize(name)
+         @name=name
+         @@all << self
+      end
+
+      def self.all
+         @@all
+      end
 
       def new_appointment(doctor,date)
          Appointment.new(date,self,doctor)
