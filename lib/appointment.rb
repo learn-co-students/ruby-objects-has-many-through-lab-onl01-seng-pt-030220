cd3f37a369de7863a8c09e553ada8d26aad9ae20
jreyes8631 <jreyes8631@gmail.com>
2020-04-06 18:55:38 -0400

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,20 +1,16 @@
 class Appointment
-
   attr_accessor :date, :patient, :doctor
   @@all = []
-
   def initialize(date, patient, doctor)
-    @date = date
+    @date = "Monday, August 1st"
     @patient = patient
     @doctor = doctor
     @@all << self
     save
   end
-
   def self.all
     @@all
   end
-
   def save
    @@all << self
   end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,35 +1,32 @@
-
 class Patient
-
+  #body of a patient instance
   attr_accessor :name, :appointment, :doctor
-
-  @@all = []
-
+  @@all_appointment = []
+  # Initialize with a name 
   def initialize(name)
     @name = name
-    @@all << self
+    @appointment = appointment
+    @doctor = doctor
+    @@all_appointment << self
     save
   end
-
+  # Knows all patients
   def self.all
-    @@all
+    @@all_appointment
   end
-  #appointments, that iterates through the Appointments array and returns Appointments that belong to the patient.
-
+  # returns all appointments associated with this Patient
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
+    # Appointment.all.select {|appointment| appointment.doctor == self}
   end
-
   def doctors
     appointments.collect {|appointment| appointment.doctor}
   end
-
-  def new_appointment(date, doctor)
-    Appointment.new(date, doctor, self)
+  # giving a doctor and date, creates a new appointment belonging to that patient
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
-  
   def save
-   @@all << self
+    @@all_appointment << self
   end
-
 end
